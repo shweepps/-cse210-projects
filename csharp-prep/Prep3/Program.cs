6f597074f1e48a273837c1461ef4eb2fe7dffc84
shweepps <shweepps@gmail.com>
2023-04-27 09:46:10 +0200

    prep3-5 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        // Loop until the user guesses the magic number
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break; // Exit the loop
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int num;
+        do {
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0) {
+                numbers.Add(num);
+            }
+        } while (num != 0);
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }

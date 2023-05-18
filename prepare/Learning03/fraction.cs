21b1641fc9b5bd78ba5c1385ffea2bcf7ef39839
shweepps <shweepps@gmail.com>
2023-05-18 08:05:02 +0200

    lesson3

--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -7,7 +7,6 @@ public class Fraction
 
     public Fraction()
     {
-        // Default to 1/1
         _top = 1;
         _bottom = 1;
     }
@@ -26,16 +25,12 @@ public class Fraction
 
     public string GetFractionString()
     {
-        // Notice that this is not stored as a member variable.
-        // Is is just a temporary, local variable that will be recomputed each time this is called.
         string text = $"{_top}/{_bottom}";
         return text;
     }
 
     public double GetDecimalValue()
     {
-        // Notice that this is not stored as a member variable.
-        // Is will be recomputed each time this is called.
         return (double)_top / (double)_bottom;
     }
 }
